3ddde32cb9c419b959066f4a1b49971a5d14b427
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2017-11-30 16:15:22 +0000

    God, I forgot to commit all those changes in those support files!

---
+++ OPL2LPT/timer.h
@@ -0,0 +1,12 @@
+#ifndef TIMER_H_
+#define TIMER_H_
+
+void timer_setup(unsigned short frequency);
+void timer_shutdown();
+unsigned long timer_get();
+void timer_reset(unsigned short frequency);
+
+void hlt(void);
+#pragma aux hlt = "hlt";
+
+#endif
