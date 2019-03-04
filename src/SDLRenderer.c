9493c68f816772343af92be7d52cc0eec1b636fa
Daniel Monteiro <danielmonteiro@id.uff.br>
2019-03-04 23:40:48 +0000

    Fix SDL rendering

--- src/SDLRenderer.c
+++ src/SDLRenderer.c
@@ -168,34 +168,24 @@ void onQuit() {
 void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
 {
     int bpp = surface->format->BytesPerPixel;
-    /* Here p is the address to the pixel we want to set */
+
     Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
 
-    switch(bpp) {
-        case 1:
-            *p = pixel;
-            break;
-
-        case 2:
-            *(Uint16 *)p = pixel;
-            break;
-
-        case 3:
             if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
-                p[0] = (pixel >> 16) & 0xff;
-                p[1] = (pixel >> 8) & 0xff;
-                p[2] = pixel & 0xff;
+                *(Uint32 *)p = SDL_MapRGBA( surface->format,
+                                            ( pixel & 0xFF0000 ) >> 16,
+                                            ( pixel & 0x00FF00 ) >> 8,
+                                            ( pixel & 0x0000FF ),
+                                            255
+                );
             } else {
-                p[0] = pixel & 0xff;
-                p[1] = (pixel >> 8) & 0xff;
-                p[2] = (pixel >> 16) & 0xff;
+                *(Uint32 *)p = SDL_MapRGBA( surface->format,
+                                            ( pixel & 0x0000FF ),
+                                            ( pixel & 0x00FF00 ) >> 8,
+                                            ( pixel & 0xFF0000 ) >> 16,
+                                            255
+                        );
             }
-            break;
-
-        case 4:
-            *(Uint32 *)p = pixel;
-            break;
-    }
 }
 
 void copyImageBufferToVideoMemory(uint8_t* imageBuffer ) {
